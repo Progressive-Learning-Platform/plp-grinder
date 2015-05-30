35ddba676af0e15962217af7bcae7f56b7e9b744
Zach Moore <zcmoore@asu.edu>
2015-05-30 13:27:10 -0700

    Add comment complexity

--- sampleData/BasicArithmatic.java
+++ sampleData/BasicArithmatic.java
@@ -1,7 +1,7 @@
 public class BasicArithmatic
 {
 	private static final int constant = 10;
-
+	
 	public static void main(String[] args)
 	{
 		int a = 4;
@@ -13,5 +13,16 @@ public class BasicArithmatic
 		int subImmediate = a - 2;
 		int addConstant = b * constant;
 		int multiplyImmediate = b * 5;
+		// DO NOT LEX
+		/* Do not lex */
+		/*
+		 * Do not lex 2
+		 */
+		// /**/ Do not lex 3
+		/*
+		 * 
+		 * // */
+		// Strings are currently unsupported
+		// String s = "/*LexAstring" + lex + "LexAstring*/ LexAstring";
 	}
 }
