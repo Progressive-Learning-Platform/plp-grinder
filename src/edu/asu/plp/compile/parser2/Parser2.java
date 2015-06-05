0ce1acfa637899fb44a44a7af40da60d0965014d
Zach Moore <zcmoore@asu.edu>
2015-06-05 15:17:39 -0700

    Initial overhaul (parser)

---
+++ src/edu/asu/plp/compile/parser2/Parser2.java
@@ -0,0 +1,16 @@
+package edu.asu.plp.compile.parser2;
+
+public class Parser2
+{
+	
+	
+	private void parsePackage()
+	{
+		// TODO
+	}
+	
+	private void parseImports()
+	{
+		// TODO
+	}
+}
---
+++ src/edu/asu/plp/compile/parser2/Signature.java
@@ -0,0 +1,16 @@
+package edu.asu.plp.compile.parser2;
+
+import java.util.List;
+
+public class Signature
+{
+	private List<String> exceptions;
+	private List<String> argumentTypes;
+	private String methodName;
+	private String returnType;
+	private String accessModifier;
+	private boolean isStatic;
+	private List<String> genericArguments;
+	
+	
+}
