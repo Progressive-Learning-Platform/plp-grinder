486042c274be53cb4b71917ef6cfdf300d952324
Zach Moore <zcmoore@asu.edu>
2015-06-07 11:51:33 -0700

    Block infrastructure

---
+++ src/edu/asu/plp/compile/parser/segments/Block.java
@@ -0,0 +1,6 @@
+package edu.asu.plp.compile.parser.segments;
+
+public class Block implements Segment
+{
+	
+}
---
+++ src/edu/asu/plp/compile/parser/segments/UnitSegment.java
@@ -0,0 +1,6 @@
+package edu.asu.plp.compile.parser.segments;
+
+public interface UnitSegment extends Segment
+{
+	
+}
