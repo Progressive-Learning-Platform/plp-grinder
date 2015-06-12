3c66ee593368477475f4dd3020821f2d0c04e4d1
Zach Moore <zcmoore@asu.edu>
2015-06-11 17:39:37 -0700

    Flush constructs

--- src/edu/asu/plp/compile/parser/constructs/MethodConstruct.java
+++ src/edu/asu/plp/compile/parser/constructs/MethodConstruct.java
@@ -1,11 +1,35 @@
 package edu.asu.plp.compile.parser.constructs;
 
-import java.util.List;
-
-import edu.asu.plp.compile.parser.Variable;
+import edu.asu.plp.compile.parser.tree.ParseNode;
 
 public class MethodConstruct
 {
-	private List<Variable> arguments;
-	private List<Variable> localVariables;
+	private Signature signature;
+	private ParseNode body;
+	
+	public MethodConstruct(Signature signature)
+	{
+		super();
+		this.signature = signature;
+	}
+	
+	public Signature getSignature()
+	{
+		return signature;
+	}
+	
+	public void setSignature(Signature signature)
+	{
+		this.signature = signature;
+	}
+	
+	public ParseNode getBody()
+	{
+		return body;
+	}
+	
+	public void setBody(ParseNode body)
+	{
+		this.body = body;
+	}
 }
--- src/edu/asu/plp/compile/parser/constructs/Signature.java
+++ src/edu/asu/plp/compile/parser/constructs/Signature.java
@@ -1,16 +1,125 @@
 package edu.asu.plp.compile.parser.constructs;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Signature
 {
 	private List<String> exceptions;
-	private List<String> argumentTypes;
+	private List<Variable> arguments;
 	private String methodName;
 	private String returnType;
 	private String accessModifier;
 	private boolean isStatic;
 	private List<String> genericArguments;
 	
+	public Signature()
+	{
+		this.exceptions = new ArrayList<>();
+		this.arguments = new ArrayList<>();
+		this.genericArguments = new ArrayList<>();
+	}
 	
+	public Signature(String methodName, String returnType, String accessModifier)
+	{
+		this();
+		this.methodName = methodName;
+		this.returnType = returnType;
+		this.accessModifier = accessModifier;
+	}
+
+	public void addException(String exception)
+	{
+		exceptions.add(exception);
+	}
+	
+	public void addArgument(Variable argument)
+	{
+		arguments.add(argument);
+	}
+	
+	public void addArgument(String type, String name, boolean constant)
+	{
+		addArgument(new Variable(type, name, constant));
+	}
+	
+	public void addArgument(String type, String name)
+	{
+		addArgument(new Variable(type, name));
+	}
+	
+	public void addGenericArgument(String genericArgument)
+	{
+		genericArguments.add(genericArgument);
+	}
+
+	public List<String> getExceptions()
+	{
+		return exceptions;
+	}
+
+	public void setExceptions(List<String> exceptions)
+	{
+		this.exceptions = exceptions;
+	}
+
+	public List<Variable> getArguments()
+	{
+		return arguments;
+	}
+
+	public void setArguments(List<Variable> arguments)
+	{
+		this.arguments = arguments;
+	}
+
+	public String getMethodName()
+	{
+		return methodName;
+	}
+
+	public void setMethodName(String methodName)
+	{
+		this.methodName = methodName;
+	}
+
+	public String getReturnType()
+	{
+		return returnType;
+	}
+
+	public void setReturnType(String returnType)
+	{
+		this.returnType = returnType;
+	}
+
+	public String getAccessModifier()
+	{
+		return accessModifier;
+	}
+
+	public void setAccessModifier(String accessModifier)
+	{
+		this.accessModifier = accessModifier;
+	}
+
+	public boolean isStatic()
+	{
+		return isStatic;
+	}
+
+	public void setStatic(boolean isStatic)
+	{
+		this.isStatic = isStatic;
+	}
+
+	public List<String> getGenericArguments()
+	{
+		return genericArguments;
+	}
+
+	public void setGenericArguments(List<String> genericArguments)
+	{
+		this.genericArguments = genericArguments;
+	}
 }
