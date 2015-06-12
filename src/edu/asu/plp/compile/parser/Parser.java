ede0d0885e8b5f7c8be0c393450abd4a048f04b9
Zach Moore <zcmoore@asu.edu>
2015-06-11 19:58:05 -0700

    Revert "Perge"
    This reverts commit bb0afc1ae1b851e88cf6321984ddd59acf8ebbac.

--- src/edu/asu/plp/compile/parser/Parser.java
+++ src/edu/asu/plp/compile/parser/Parser.java
@@ -3,15 +3,39 @@ package edu.asu.plp.compile.parser;
 import java.util.List;
 
 import edu.asu.plp.Token;
-import edu.asu.plp.compile.parser.tree.ParseNode;
 
 public class Parser
 {
 	private List<Token> tokens;
+	private String tokenString;
 	
 	public Parser(List<Token> tokens)
 	{
 		this.tokens = tokens;
+		this.tokenString = stringify(tokens);
+	}
+	
+	private String stringify(List<Token> tokens)
+	{
+		StringBuilder stringBuilder = new StringBuilder();
+		
+		Token first = tokens.get(0);
+		stringBuilder.append(stringify(first));
+		
+		for (int index = 1; index < tokens.size(); index++)
+		{
+			Token token = tokens.get(index);
+			stringBuilder.append(" ");
+			stringBuilder.append(stringify(token));
+		}
+		
+		return stringBuilder.toString();
+	}
+	
+	private String stringify(Token token)
+	{
+		// FIXME: this algorithm breaks if the token value contains spaces
+		return token.getType().name() + "-" + token.getValue();
 	}
 	
 	private void parsePackage()
@@ -29,10 +53,19 @@ public class Parser
 		
 	}
 	
-	private ParseNode parseMethod()
+	private void parseMethod()
 	{
+		String declarationRegex = "(TYPE-\\S*) (REFERNCE-\\S*) (CONTROL-;)";
+		// TODO: support parenthesis scopes (e.g. var = a + (b * c) - (b + a))
+		String assignmentRegex = "(REFERNCE-\\S*) (OPERATOR_BINARY-=) "
+				+ "("
+					+ "((REFERNCE-\\S*)|(LITERAL_\\S*))"
+					+ "( (OPERATOR_BINARY-[^\\s=]*) ((REFERNCE-\\S*)|(LITERAL_\\S*)))*"
+				+ ")" + "(CONTROL-;)";
+		String declareAndAssignRegex = "(TYPE-\\S*) )" + assignmentRegex;
+		String methodRegex = "(REFERNCE-\\S*) (CONTROL-\\()"
+				+ "";
 		
-		return null;
 	}
 	
 	public static void main(String[] args)
