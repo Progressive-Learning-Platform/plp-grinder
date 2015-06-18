57cfafd27c91acc1cb095aa3dd51712576b16c48
Morgan Nesbitt <nmorgan1214@gmail.com>
2015-06-17 21:06:32 -0700

    Parse expression and declaration
    Note: public for testing purposes
    TODO: accept urnary operator based statements (non binary operator
    statments). / Generalize

--- src/edu/asu/plp/compile/parser/Parser.java
+++ src/edu/asu/plp/compile/parser/Parser.java
@@ -1,8 +1,18 @@
 package edu.asu.plp.compile.parser;
 
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
 import java.util.List;
 
 import edu.asu.plp.Token;
+import edu.asu.plp.Token.Type;
+import edu.asu.plp.compile.lex.LexException;
+import edu.asu.plp.compile.parser.constructs.ClassConstruct;
+import edu.asu.plp.compile.parser.support.TreeSupport;
+import edu.asu.plp.compile.parser.tree.DeclarationNode;
+import edu.asu.plp.compile.parser.tree.ParseNode;
+import edu.asu.plp.compile.parser.tree.SequenceNode;
+import edu.asu.plp.compile.parser.tree.ValueNode;
 
 public class Parser
 {
@@ -68,9 +78,40 @@ public class Parser
 		
 	}
 	
-	public static void main(String[] args)
-	{
+	//TODO Change public to private. Testing purposes
+	public SequenceNode parseArithmeticStatement(List<Token> sequenceTokens) throws ParseException
+	{	
+		ParseExpression tree = new ParseExpression(sequenceTokens);
 		
+		return tree.parse();
+	}
+
+	//TODO Change public to private. Testing purposes
+	public SequenceNode parseDeclaration(LinkedList<Token> declarationList) throws ParseException
+	{
+		SequenceNode declaration = new SequenceNode();
+		
+		List<ParseNode> declarationNodes = declaration.getChildren();
+		
+		int starter = 0;
+		ValueNode originalType = new ValueNode(declarationList.get(starter++).getValue());
+		
+		for(int index = starter; index < declarationList.size(); index++)
+		{
+			if(declarationList.get(index).getType() == Type.REFERNCE)
+			{
+				DeclarationNode declarationNode = new DeclarationNode();
+				List<ParseNode> children = declarationNode.getChildren();
+				TreeSupport.insertChildAt(children, 0, originalType);
+				TreeSupport.insertChildAt(children, 1, new ValueNode(declarationList.get(index).getValue()));
+				declarationNodes.add(declarationNode);
+			}
+		}
+		return declaration;
+	}
+	
+	public static void main(String[] args) throws FileNotFoundException, LexException, ParseException
+	{
 	}
 	
 }
