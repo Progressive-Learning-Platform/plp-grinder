c9a97fb677a3ec5079b190dc13dc3ebb642cdfa7
Zach Moore <zcmoore@asu.edu>
2015-07-01 18:17:18 -0700

    Parser structure

--- src/rust/parser/mod.rs
+++ src/rust/parser/mod.rs
@@ -1,4 +1,32 @@
 mod tree;
+use std::collections::HashMap;
+use parser::tree::ParseTree;
+
+pub struct BodyParser<'a>
+{
+	pub variables: HashMap<Variable<'a>, &'a str>,
+	pub body: ParseTree<'a>,
+}
+
+impl<'a> BodyParser<'a>
+{
+	fn parse() -> Option<ParseResult<'a>>
+	{
+		None
+	}
+}
+
+pub struct ParseResult<'a>
+{
+	end_index: usize,
+	tree: ParseTree<'a>,
+}
+
+pub struct Variable<'a>
+{
+	pub name: &'a str,
+	pub vtype: &'a str,
+}
 
 pub fn get_start_symbols<'a>() -> Vec<&'a str>
 {
