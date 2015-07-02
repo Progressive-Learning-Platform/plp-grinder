368ccf0a9f671841bc886c49dbacc02b3c4ef2aa
Zach Moore <zcmoore@asu.edu>
2015-07-01 18:21:13 -0700

    Parser start

--- src/rust/main.rs
+++ src/rust/main.rs
@@ -5,18 +5,133 @@ mod support;
 mod tokens;
 mod lexer;
 mod files;
+mod parser;
 
 use std::vec::Vec;
 use tokens::*;
 use lexer::*;
+use parser::*;
 
 fn main()
 {
     let source_file = "sampleData/BasicArithmatic.java";
     let lex_output_file = "sampleData/output/stable/BasicArithmatic.java.lexed";
+    let preprocessed_output_file = "sampleData/output/stable/BasicArithmatic.java.preprocessed";
 
     let mut tokens: Vec<Token> = lex_file(source_file);
 
     println!("\n\nFound Tokens:");
     tokens.print_to(lex_output_file, true);
+
+    remove_meta(&mut tokens);
+    println!("\n\nPerged Tokens:");
+    tokens.print_to(preprocessed_output_file, true);
+
+    let invalid_types = get_invalid_token_types();
+    let invalid_values = get_invalid_token_values();
+
+    let min_index = 0;
+    for (index, token) in tokens.iter().enumerate()
+    {
+        if index < min_index
+        {
+            continue;
+        }
+
+        if invalid_types.contains(&token.name)
+        {
+            panic!("Unsupported token type: {}", token.name);
+        }
+        else if invalid_values.contains(&&*token.value)
+        {
+            panic!("Unsupported token value: {}", token.value);
+        }
+
+        if token.value == "class"
+        {
+            // parse class body
+            let (string, unused) = class_to_plp(&tokens, index);
+        }
+        /*else if token.name == "type" // || token.type == "identifier"
+        {
+            // look ahead
+            // parse variable declaration
+            // OR parse method declaration
+        }
+        else if token.value == "if"
+        {
+            // parse if body
+            // Unsupported for now
+        }
+        else if token.name == "construct.conditional"
+        {
+            // parse conditional
+            // Unsupported for now
+        }*/
+        else
+        {
+            panic!("Unexpected token: {}\t{}", token.name, token.value);
+        }
+    }
+}
+
+fn class_to_plp(tokens: &Vec<Token>, start_index: usize) -> (usize, String)
+{
+    let mut plp_string: String = String::new();
+    let mut current_index: usize = start_index;
+
+    for (index, token) in tokens.iter().enumerate()
+    {
+        current_index = index;
+    }
+
+    (current_index + 1, plp_string)
+}
+
+fn remove_meta(tokens: &mut Vec<Token>)
+{
+    let mut invalid_indecies: Vec<usize> = Vec::new();
+
+    let mut min_index: usize = 0;
+    for (index, token) in tokens.iter().enumerate()
+    {
+        if index < min_index
+        {
+            invalid_indecies.push(index);
+        }
+        else if token.name == "special.import"
+        {
+            invalid_indecies.push(index);
+            min_index = index + 2; // skip the next token (semi-colon)
+        }
+        else if token.name.starts_with("comment")
+        {
+            invalid_indecies.push(index);
+        }
+        // Ignore all modifiers (public, private, volatile, transient, static, etc.)
+        else if token.name.starts_with("mod")
+        {
+            invalid_indecies.push(index);
+        }
+        else if token.name == "special.package"
+        {
+            invalid_indecies.push(index);
+            min_index = find_next_semicolon(tokens, index) + 1;
+        }
+    }
+
+    for (count, index) in invalid_indecies.iter().enumerate()
+    {
+        tokens.remove(index - count);
+    }
+}
+
+fn find_next_semicolon(tokens: &Vec<Token>, start: usize) -> usize
+{
+    for (index, token) in tokens[start..].iter().enumerate()
+    {
+        if token.value == ";" { return index + start; }
+    }
+
+    return 0;
 }
