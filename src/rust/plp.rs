86a1ddba0298bcb74e27409e22711046f3935dc9
Zach Moore <zcmoore@asu.edu>
2015-07-08 12:08:09 -0700

    Implement indentation writing

--- src/rust/plp.rs
+++ src/rust/plp.rs
@@ -13,7 +13,6 @@ pub struct PLPWriter
 
 impl PLPWriter
 {
-	// TODO: implement indentation writing
 	pub fn new() -> PLPWriter
 	{
 		PLPWriter {
@@ -21,6 +20,22 @@ impl PLPWriter
 			indent_level: 0,
 			code: String::new(),
 			}
+	}
+
+	fn create_indented_string(&self) -> String
+	{
+		let mut string = String::new();
+		let indent = match self.use_tabs {
+				true  => "\t",
+				false => " ",
+			};
+
+		for index in (0..self.indent_level)
+		{
+			string.push_str(indent);
+		}
+
+		string
 	}
 
 	pub fn reset(&mut self)
@@ -31,7 +46,7 @@ impl PLPWriter
 
 	pub fn li(&mut self, register: &str, value: &str) -> String
 	{
-		let mut code = String::new();
+		let mut code = self.create_indented_string();
 		code.push_str("li ");
 		code.push_str(register);
 		code.push_str(", ");
@@ -44,7 +59,7 @@ impl PLPWriter
 
 	pub fn mov(&mut self, register_to: &str, register_from: &str) -> String
 	{
-		let mut code = String::new();
+		let mut code = self.create_indented_string();
 		code.push_str("move ");
 		code.push_str(register_to);
 		code.push_str(", ");
@@ -57,7 +72,7 @@ impl PLPWriter
 
 	pub fn addu(&mut self, register_sum: &str, register_addend1: &str, register_addend2: &str) -> String
 	{
-		let mut code = String::new();
+		let mut code = self.create_indented_string();
 		code.push_str("addu ");
 		code.push_str(register_sum);
 		code.push_str(", ");
@@ -72,7 +87,7 @@ impl PLPWriter
 
 	pub fn subu(&mut self, register_difference: &str, register_addend1: &str, register_addend2: &str) -> String
 	{
-		let mut code = String::new();
+		let mut code = self.create_indented_string();
 		code.push_str("subu ");
 		code.push_str(register_difference);
 		code.push_str(", ");
@@ -87,7 +102,7 @@ impl PLPWriter
 
 	pub fn mullo(&mut self, register_product: &str, register_multiplicand1: &str, register_multiplicand2: &str) -> String
 	{
-		let mut code = String::new();
+		let mut code = self.create_indented_string();
 		code.push_str("mullo ");
 		code.push_str(register_product);
 		code.push_str(", ");
@@ -102,7 +117,7 @@ impl PLPWriter
 
 	pub fn sw(&mut self, register_target: &str, offset: u16, register_address: &str) -> String
 	{
-		let mut code = String::new();
+		let mut code = self.create_indented_string();
 		code.push_str("sw ");
 		code.push_str(register_target);
 		code.push_str(", ");
@@ -117,7 +132,7 @@ impl PLPWriter
 
 	pub fn lw(&mut self, register_target: &str, offset: u16, register_address: &str) -> String
 	{
-		let mut code = String::new();
+		let mut code = self.create_indented_string();
 		code.push_str("lw ");
 		code.push_str(register_target);
 		code.push_str(", ");
@@ -132,7 +147,7 @@ impl PLPWriter
 
 	pub fn push(&mut self, register_target: &str) -> String
 	{
-		let mut code = String::new();
+		let mut code = self.create_indented_string();
 		code.push_str("push ");
 		code.push_str(register_target);
 		code.push_str("\n");
@@ -143,7 +158,7 @@ impl PLPWriter
 
 	pub fn pop(&mut self, register_target: &str) -> String
 	{
-		let mut code = String::new();
+		let mut code = self.create_indented_string();
 		code.push_str("pop ");
 		code.push_str(register_target);
 		code.push_str("\n");
@@ -154,7 +169,7 @@ impl PLPWriter
 
 	pub fn call(&mut self, function_label: &str) -> String
 	{
-		let mut code = String::new();
+		let mut code = self.create_indented_string();
 		code.push_str("call ");
 		code.push_str(function_label);
 		code.push_str("\n");
@@ -167,7 +182,7 @@ impl PLPWriter
 
 	pub fn nop(&mut self) -> String
 	{
-		let mut code = String::new();
+		let mut code = self.create_indented_string();
 		code.push_str("nop");
 		code.push_str("\n");
 
@@ -177,7 +192,7 @@ impl PLPWriter
 
 	pub fn label(&mut self, label: &str) -> String
 	{
-		let mut code = String::new();
+		let mut code = self.create_indented_string();
 		code.push_str(label);
 		code.push_str(":");
 		code.push_str("\n");
@@ -188,7 +203,7 @@ impl PLPWriter
 
 	pub fn beq(&mut self, register_comparator1: &str, register_comparator2: &str, target_label: &str) -> String
 	{
-		let mut code = String::new();
+		let mut code = self.create_indented_string();
 		code.push_str("beq ");
 		code.push_str(register_comparator1);
 		code.push_str(", ");
@@ -203,7 +218,7 @@ impl PLPWriter
 
 	pub fn bne(&mut self, register_comparator1: &str, register_comparator2: &str, target_label: &str) -> String
 	{
-		let mut code = String::new();
+		let mut code = self.create_indented_string();
 		code.push_str("bne ");
 		code.push_str(register_comparator1);
 		code.push_str(", ");
@@ -218,7 +233,7 @@ impl PLPWriter
 
 	pub fn ret(&mut self) -> String
 	{
-		let mut code = String::new();
+		let mut code = self.create_indented_string();
 		code.push_str("return\nnop\n");
 
 		self.code.push_str(&*code);
@@ -227,7 +242,7 @@ impl PLPWriter
 
 	pub fn j(&mut self, label: &str) -> String
 	{
-		let mut code = String::new();
+		let mut code = self.create_indented_string();
 		code.push_str("j ");
 		code.push_str(label);
 		code.push_str("\n");
@@ -239,7 +254,7 @@ impl PLPWriter
 
 	pub fn space(&mut self, amount: u16) -> String
 	{
-		let mut code = String::new();
+		let mut code = self.create_indented_string();
 		code.push_str(".space ");
 		code.push_str(&*amount.to_string());
 		code.push_str("\n");
