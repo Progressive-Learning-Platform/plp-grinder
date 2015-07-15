e7b4b014bdba68793839e3fefacfefed8a09835e
Zach Moore <zcmoore@asu.edu>
2015-07-15 09:00:49 -0700

    Do not preface 0 scope class labels with "_"
    E.g. use "BasicArithmatic_static:" instead of "_BasicArithmatic_static:"

--- src/rust/compiler/symbol_analysis.rs
+++ src/rust/compiler/symbol_analysis.rs
@@ -7,18 +7,18 @@ use plp::PLPWriter;
 /// @return: (static_memory_label, static_init_label, local_init_label)
 pub fn get_class_labels(class_symbol: &Symbol) -> (String, String, String)
 {
-    let mut static_memory_label = class_symbol.namespace.clone();
-    static_memory_label.push_str("_");
-    static_memory_label.push_str(&*class_symbol.name.clone());
-    static_memory_label.push_str("_static");
+    let mut namespace_label = class_symbol.namespace.clone();
+    if !namespace_label.is_empty() { namespace_label.push_str("_"); }
+    namespace_label.push_str(&*class_symbol.name.clone());
 
-    let mut local_init_label = class_symbol.namespace.clone();
-    local_init_label.push_str("_");
-    local_init_label.push_str(&*class_symbol.name.clone());
-    local_init_label.push_str("_local_init");
+    let mut static_memory_label = namespace_label.clone();
+    static_memory_label.push_str("_static");
 
     let mut static_init_label = static_memory_label.to_string();
     static_init_label.push_str("_init");
+
+    let mut local_init_label = namespace_label.clone();
+    local_init_label.push_str("_local_init");
 
     (static_memory_label, static_init_label, local_init_label)
 }
