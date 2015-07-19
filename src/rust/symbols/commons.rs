a81721e82c48e102d1c6b30ae2d7ee4451971d40
Morgan Nesbitt <nmorgan1214@gmail.com>
2015-07-19 11:08:29 -0700

    Unified Namespace and label delimiters
    Fixes #20

--- src/rust/symbols/commons.rs
+++ src/rust/symbols/commons.rs
@@ -2,10 +2,20 @@
 /// Combine a namespace and a body name to create a new namespace name.
 /// For instance, "OuterClass" and "InnerClass" can be concatenated to obtain the complete namespace of InnerClass
 /// @return the concatenated namespace
+pub fn namespace_delimiter<'a>() -> &'a str
+{
+	"."
+}
+
+pub fn label_delimiter<'a>() -> &'a str
+{
+	"_"
+}
+
 pub fn concatenate_namespace(namespace: &str, extension: &str) -> String
 {
 	let mut new_namespace = namespace.to_string();
-	new_namespace.push_str(".");
+	new_namespace.push_str(namespace_delimiter());
 	new_namespace.push_str(extension);
 
     return new_namespace;
@@ -16,7 +26,7 @@ pub fn concatenate_namespace(namespace: &str, extension: &str) -> String
 pub fn concatenate_label(label: &str, extension: &str) -> String
 {
 	let mut new_label = label.to_string();
-	new_label.push_str("_");
+	new_label.push_str(label_delimiter());
 	new_label.push_str(extension);
 
     return new_label;
@@ -26,5 +36,5 @@ pub fn concatenate_label(label: &str, extension: &str) -> String
 /// @return the concatenated label
 pub fn create_label_from_namespace(namespace: &str) -> String
 {
-    return namespace.to_string().replace(".", "_");
+    return namespace.to_string().replace(namespace_delimiter(), label_delimiter());
 }
